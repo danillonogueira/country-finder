4bd85c87fcab28fd52611aca7db38e8996b6e290
Danillo Lucas Nogueira <dnll_nog@yahoo.com>
2021-02-15 19:23:45 -0300

    changed to hooks approach

--- src/components/display.js
+++ src/components/display.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import fetchCountries from '../helpers/service';
 import Card from './card';
-import styled from 'styled-components';
 
 const StyledDisplay = styled.div`
   display: grid;
@@ -9,31 +9,26 @@ const StyledDisplay = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
 `;
 
-class Display extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { countries: [] }
-  }
+const Display = () => {
+  const [ countries, setCountries ] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     fetchCountries()
       .then(res => res.json())
-      .then(countries => this.setState({ countries }))
+      .then(countries => setCountries(countries))
       .catch(err => console.log(err));
-  }
+  });
 
-  getCountries() {
-    return this.state.countries.map((country, index) => {          
-      return <Card 
-        country={country} 
-        key={index + 1}
-      ></Card>;
-    });
-  }
-
-  render() {
-    return <StyledDisplay>{this.getCountries()}</StyledDisplay>;
-  }
-}
+  return <StyledDisplay>
+    {
+      countries.map((country, index) => {          
+        return <Card 
+          country={country} 
+          key={index + 1}
+        ></Card>;
+      })
+    }
+  </StyledDisplay>;
+};
 
 export default Display;
